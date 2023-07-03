54153fc78af6b9ac80e8ff5345f3cf0561a166c3
Uncried <egorbaranov85@gmail.com>
2023-07-03 21:09:40 +0300

    lastt

--- Compiled-HTML/PALAC/app/assets/scripts/indexpage.js
+++ Compiled-HTML/PALAC/app/assets/scripts/indexpage.js
@@ -1,25 +1,31 @@
-$('.center').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
+$(document).ready(function () {
+
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        dots: true,
+        infinite: false,
+        slidesToShow: 2,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
             }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }
-    ]
-});
+        ]
+    });
+})
+
