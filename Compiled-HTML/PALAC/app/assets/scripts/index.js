bc1b3b82bfcea1d2a1a52973a47411a921e2456a
iamzzsw <114995298+iamzzsw@users.noreply.github.com>
2023-07-06 21:08:46 +0300

    menu

--- Compiled-HTML/PALAC/app/assets/scripts/index.js
+++ Compiled-HTML/PALAC/app/assets/scripts/index.js
@@ -15,26 +15,32 @@ function callbackFunc() {
 }
 
 const burger_button = document.querySelector(".icon-burger");
+const icon_close = document.querySelector(".icon-close");
 const menu = document.querySelector('.layout-header__right')
 console.log(burger_button);
 burger_button.addEventListener('click', () => {
     menu.classList.toggle('menu');
-    if(menu.classList.contains("menu")){
-        layout.style.top = '3.3rem';
-    } else {
-    layout.style.top = '0';}
+    burger_button.style.display = "none";
+    icon_close.style.display = "block";
+})
+
+icon_close.addEventListener('click', () => {
+  menu.classList.toggle('menu')
+  burger_button.style.display = "block";
+  icon_close.style.display = "none";
 })
 
 const burger_button_dropdown = document.querySelector(".icon-burger-navbar");
+const icon_close_dropdown = document.querySelector(".icon-close-navbar");
 
 
 burger_button_dropdown.addEventListener('click', () => {
     menu.classList.toggle('menu')
-    // let y = window.pageYOffset;
-    // layout.style.top = `${y}px`;
-    // if (menu.classList.contains("menu")) {
-    //     drop_doun_header.style.top = "0";
-    //   } else {
-    //     drop_doun_header.style.top = "-8rem";
-    // }
+    burger_button_dropdown.style.display = "none";
+    icon_close_dropdown.style.display = "block";
+})
+icon_close_dropdown.addEventListener('click', () => {
+  menu.classList.toggle('menu')
+  burger_button_dropdown.style.display = "block";
+  icon_close_dropdown.style.display = "none";
 })
